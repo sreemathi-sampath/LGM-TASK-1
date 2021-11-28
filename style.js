1583eddb8b0d69ba2f26189b90bf3c60d7e60174
Sreemathi Sampath <88151472+sreemathi-sampath@users.noreply.github.com>
2021-11-28 22:54:05 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,509 @@
+body {
+    overflow-x: hidden;
+    background-color: #fad5aa;
+    margin-left: 0%;
+    margin-right: 0%;
+  }
+  .nav {
+    position: fixed;
+  }
+  
+  .bh {
+    padding-right: 45%;
+  }
+  .font {
+    font-family: "Ubuntu";
+    font-weight: bold;
+  }
+  .carousel .carousel-indicators li {
+    width: 5px;
+    height: 5px;
+    border-radius: 100%;
+  }
+  .container-fluid {
+    padding: 3% 23% 6%;
+  }
+  .navbar-brand {
+    font-size: 2.7rem;
+    position: absolute;
+    z-index: 10;
+  }
+  .nav-item {
+    margin-right: 10%;
+    /* width: fit-content; */
+    z-index: 10;
+  }
+  .nav-link {
+    font-size: 0.8rem;
+    z-index: 10;
+  }
+  .nav-link:hover {
+    transform: scale(1.3);
+    transition: transform 0.4s;
+    color: rgb(0, 190, 133) !important;
+  }
+
+  .navbar-toggler,
+  .collapse {
+    position: relative;
+    z-index: 10;
+    left: 40%;
+    text-align: left;
+  }
+
+  .crd3:hover {
+    transform: scale(1.2);
+    z-index: 20;
+    transition: transform 0.6s;
+  }
+  .crd4{
+    background-color: lightcyan;
+    border-style: hidden;
+    border-radius: 5%;
+  }
+  .crd4:hover {
+    transform: scale(1.2);
+    z-index: 20;
+    transition: transform 0.6s;
+  }
+  .visible {
+    color: white;
+    position: relative;
+    font-size: 30px;
+    padding-top: 50px;
+    top: 65%;
+    z-index: 10;
+  }
+  .visible_2 {
+    font-family: "Ubuntu";
+    font-size: 1.1rem;
+    color: white;
+    position: relative;
+    top: 80%;
+    z-index: 10;
+  }
+  .butn {
+    position: relative;
+    z-index: 10;
+    top: 90%;
+    width: 40%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3%;
+    padding-bottom: 3%;
+  }
+  .butn2 {
+    position: relative;
+    z-index: 10;
+    top: 90%;
+    width: 40%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3%;
+    padding-bottom: 3%;
+  }
+  
+  .butn:hover,
+  .butn2:hover,
+  .butn3:hover,
+  .butn4:hover,
+  .butn5:hover,
+  .butn6:hover,
+  .butnn:hover,
+  .butn10:hover {
+    background-color: rgb(140, 206, 236);
+    border-color: rgb(140, 206, 236);
+    color: black;
+  }
+  .butn7:hover,
+  .butn8:hover,
+  .butn9:hover{
+    background-color: yellow;
+  }
+  .head2 {
+    padding-top: 5%;
+    text-align: center;
+  }
+  .head3 {
+    width: 50%;
+    padding-top: 8%;
+    font-family: "Ubuntu";
+    font-weight: lighter;
+    color: dimgray;
+  }
+  
+  .bel {
+    transform: rotate(30deg);
+    border-radius: 100%;
+    color: white;
+    background-color: #F38CFC;
+    padding: 8% 10% 9% 9%;
+    margin-top: 20px;
+  }
+  .ico {
+    border-radius: 100%;
+    color: white;
+    background-color: #F38CFC;
+    padding: 8% 10% 9% 9%;
+    margin-top: 20px;
+  }
+  .crd {
+    border-style: hidden;
+    margin-bottom: 5%;
+    background: white;
+    border-radius: 4%;
+  }
+  .crd3 {
+    background: linear-gradient(10deg, #313131, #dddadb);
+    border-style: hidden;
+  }
+  .title {
+    text-align: center;
+    font-size: 1rem;
+    color: rebeccapurple;
+  }
+  .title2 {
+    text-align: left;
+    font-size: 1.3rem;
+    padding-left: 10%;
+  }
+  
+  .butn3 {
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 56%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3.5%;
+    padding-bottom: 3.5%;
+  }
+  .butn7{
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 60%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 4.5%;
+    padding-bottom: 4.5%;
+    background-color:gainsboro;
+  }
+  .butn4 {
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 56%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3.5%;
+    padding-bottom: 3.5%;
+  }
+  .butn8{
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 60%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 4.5%;
+    padding-bottom: 4.5%;
+    background-color:gainsboro;
+  }
+  .butn5 {
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 56%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3.5%;
+    padding-bottom: 3.5%;
+  }
+  .butn9{
+    position: relative;
+    margin-top: 5%;
+    left: 20%;
+    width: 60%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 4.5%;
+    padding-bottom: 4.5%;
+    background-color:gainsboro;
+  }
+  .butn6 {
+    position: relative;
+    left: 10%;
+    width: 56%;
+    border-radius: 3%;
+    font-size: 0.8rem;
+    padding-top: 3.5%;
+    padding-bottom: 3.5%;
+    background-color: white;
+  }
+  .butn10{
+    font-size: 0.8rem;
+    padding-top: 1.5%;
+    padding-bottom: 1.5%;
+    width: 50%;
+    border-radius: 0;
+  }
+  .s3 {
+    margin-bottom: 10%;
+  }
+  .space {
+    margin-top: 12%;
+  }
+  .space2 {
+    padding-left: 10%;
+    color: gray;
+  }
+  .bod {
+    text-align: center;
+    font-size: smaller;
+    padding-right: 5%;
+    padding-left: 5%;
+    color: gray;
+    line-height: 1.8;
+  }
+  .bod2{
+    text-align: center;
+    font-size: smaller;
+    padding-right: 5%;
+    padding-left: 5%;
+    color: black;
+    line-height: 1.8;
+  }
+  
+
+  #video .colored-section {
+    background-color: #254BCD;
+  }
+
+
+  .vdo-section {
+    font-family: sans-serif;
+    font-weight: 700;
+    font-size: 3.5 rem;
+    line-height: 1.5;
+    padding: 50px;
+  }
+  .vdo-title{
+      padding-bottom: 100px;
+  }
+
+
+  .col-md-3 {
+    padding: 0 0 0 0;
+  }
+  #pricing {
+    padding: 0 20% 0 20%;
+  }
+  .nh {
+    margin-top: 40%;
+  }
+
+  /* ZIPPY TEAM */
+  #team {
+    padding: 60px 0;
+    text-align: center;
+    background-color: #d1d1d1;
+    color: #145889;
+  }
+  #team h2 {
+    position: relative;
+    padding: 0px 0px 15px;
+  }
+  #team p {
+    font-size: 15px;
+    font-style: italic;
+    padding: 0px;
+    margin: 25px 0px 40px;
+  }
+
+  #team .member {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: 100%;
+  }
+
+  .member-img {
+    width: 100px;
+    height: 100px;
+  }
+  
+  #team .member .member-info {
+    display: block;
+    position: relative;
+    bottom: 0px;
+    margin-top: 10px;
+    padding: 15px 0;
+  }
+  
+  #team .member h4 {
+    text-align: center;
+    font-weight: 700;
+    margin-bottom: 2px;
+    font-size: 18px;
+    color: black;
+  }
+  #team .member span {
+    font-style: italic;
+    display: block;
+    font-size: 13px;
+  }
+  #team .member .social-links {
+    align-self: center;
+    margin-top: 15px;
+  }
+  #team .member .social-links a {
+    transition: none;
+    color: black;
+  }
+  #team .member .social-links a:hover {
+    color: #7000a3;
+  }
+  #team .member .social-links i {
+    font-size: 18px;
+    margin: 0 2px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+
+/* Life at Zippy */
+
+.heading {
+  padding-top: 2%;
+  padding-bottom: 2%;
+  text-align: center;
+  font-family: "Ubuntu";
+  font-weight: bold;
+  font-size: 30px;
+}
+.zippylife-sec{
+  background-color: rgb(152, 179, 179);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 10px;
+  
+}
+.zippylife-sec-main{
+  width: 900px;
+  margin: 10px auto;
+}
+.zippylife-sec-main .zippylife-track ul{
+  position: relative;
+  list-style-type: none;
+  height: 450px;
+  width: 900px;
+}
+.zippylife-sec-main .zippylife-track ul li{
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 450px;
+  opacity: 0;
+  transition:opacity .8s ease;
+}
+.zippylife-sec-main .zippylife-track ul li:first-child{
+  opacity: 1;
+}
+.zippylife-sec-main .zippylife-track ul li img{
+  margin-left: 100px;
+  width: 80%;
+  height: 80%;
+  object-fit: cover;
+}
+.slides{
+  padding-top: 20px;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 10px;
+}
+.slides a{
+  height: 100px;
+}
+.slides a img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.zippylife-sec-main .zippylife-track ul li:target{
+  opacity: 1;
+}
+.slides a:hover{
+  border: 1.8px solid white;
+}
+
+  .container {
+    position: relative;
+    padding: 3% 23% 6%;
+  }
+  
+  .mySlides {
+    display: none;
+  }
+  
+  .cursor {
+    cursor: pointer;
+  }
+  
+  .prev {
+    cursor: pointer;
+    position: relative;
+    padding-right: 48%;
+    color: rebeccapurple;
+    font-weight: bold;
+    font-size: 20px;
+    border-radius: 0 3px 3px 0;
+    user-select: none;
+    -webkit-user-select: none;
+  }
+  
+  .next {
+    cursor: pointer;
+    position: relative;
+    padding-left: 47.6%;
+    color: rebeccapurple;
+    font-weight: bold;
+    font-size: 20px;
+    border-radius: 3px 0 0 3px;
+    user-select: none;
+    -webkit-user-select: none;
+  }
+  
+  .numbertext {
+    color: #f2f2f2;
+    font-size: 12px;
+    padding: 8px 12px;
+    position: absolute;
+    top: 0;
+  }
+  
+  .row:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+  
+  .column {
+    float: left;
+    width: 16.66%;
+  }
+  .demo {
+    opacity: 0.6;
+  }
+  
+  .active,
+  .demo:hover {
+    opacity: 1;
+  }
+  .center-block {
+      display: block;
+      margin-right: auto;
+      margin-left: auto;
+    }
